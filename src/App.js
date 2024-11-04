e99c5fe0103dc43ee0391f6808fd2dbd493190f1
pikachu <msram2006@gmail.com>
2024-11-04 22:34:54 +0530

    created random color generator with hex and rgb color!

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@
 import { useState } from 'react';
 import Accordian from './components';
 import data from "./components/data";
+import RandomColor from './components/random-color';
 
 function App() {
   return(
     <div className= "App">
-      <Accordian/>
+      {/* <Accordian/> */}
+      <RandomColor/>
     </div>
   )
 
